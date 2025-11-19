2447bf939e6330181179db97cd536de238bc8699
Alex Jercan <jercan_alex27@yahoo.com>
2025-11-19 13:22:11 +0200

    ci: trying to make the default package in flake

--- flake.nix
+++ flake.nix
@@ -3,69 +3,61 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-flake.url = "github:juspay/rust-flake";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    rust-overlay,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [(import rust-overlay)];
-      pkgs = import nixpkgs {
-        inherit system overlays;
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        # Optional: use external flake logic, e.g.
+        # inputs.foo.flakeModules.default
+        inputs.rust-flake.flakeModules.default
+        inputs.rust-flake.flakeModules.nixpkgs
+      ];
+      flake = {
+        # Put your original flake attributes here.
+      };
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        # self',
+        pkgs,
+        ...
+      }: let
+        rustNightly = pkgs.rust-bin.nightly.latest.default.override {
+          extensions = ["rust-src" "clippy" "rustfmt"];
+          targets = ["wasm32-unknown-unknown"];
+        };
+      in {
+        devShells.default = pkgs.mkShell rec {
+          nativeBuildInputs = with pkgs; [
+            openssl
+            trunk
+            wasm-pack
+            rustNightly
+            clippy
+            rust-analyzer
+            pkg-config
+            llvmPackages.bintools
+          ];
 
-        config = {
-          allowUnfree = true;
+          buildInputs = with pkgs; [
+            udev
+            alsa-lib-with-plugins
+            vulkan-loader
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr # To use the x11 feature
+            libxkbcommon
+            wayland # To use the wayland feature
+          ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          RUST_BACKTRACE = 1;
+
+          RUST_SRC_PATH = "${rustNightly}/lib/rustlib/src/rust/library";
         };
       };
-
-      rustNightly = pkgs.rust-bin.nightly.latest.default.override {
-        extensions = ["rust-src" "clippy" "rustfmt"];
-        targets = ["wasm32-unknown-unknown"];
-      };
-      manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-    in {
-      packages.default = pkgs.rustPlatform.buildRustPackage {
-        pname = manifest.name;
-        version = manifest.version;
-        cargoLock.lockFile = ./Cargo.lock;
-        src = pkgs.lib.cleanSource ./.;
-      };
-
-      devShells.default = pkgs.mkShell rec {
-        nativeBuildInputs = with pkgs; [
-          openssl
-          trunk
-          wasm-pack
-          rustNightly
-          clippy
-          rust-analyzer
-          pkg-config
-          llvmPackages.bintools
-        ];
-
-        buildInputs = with pkgs; [
-          udev
-          alsa-lib-with-plugins
-          vulkan-loader
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrandr # To use the x11 feature
-          libxkbcommon
-          wayland # To use the wayland feature
-        ];
-
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-        RUST_BACKTRACE = 1;
-
-        RUST_SRC_PATH = "${rustNightly}/lib/rustlib/src/rust/library";
-        #         RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-      };
-    });
+    };
 }
